52df7c5dfb7119fc4e06e59b5683a6a7fb98015c
Sameer Sawant <sameersawant222@gmail.com>
2015-04-09 16:00:56 +0530

    Updated file with latest repo link

--- bin/generate.rb
+++ bin/generate.rb
@@ -1,2 +1,2 @@
 # This repository has been moved to 
-https://github.com/selenium-cucumber/selenium-cucumber-ruby/master/bin/generate.rb
+https://github.com/selenium-cucumber/selenium-cucumber-ruby/edit/master/bin/generate.rb
